137651c8fd1ca42431f3a8fa54246ca37431f4e4
TheNumerus <Petr.volf216@gmail.com>
2018-04-21 00:36:37 +0200

    mesh is now generated

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,32 @@
 extern crate image;
 
+pub mod settings;
+
+use settings::{GenSettings};
 use image::{DynamicImage, GenericImage, Pixel};
 
-struct Vec3 {
-    x: f32,
-    y: f32,
-    z: f32
+#[derive(Debug)]
+pub struct Vec3 {
+    pub x: f32,
+    pub y: f32,
+    pub z: f32
 }
 
 impl Vec3 {
-    fn new(coords: (f32, f32, f32)) -> Vec3 {
+    pub fn new(coords: (f32, f32, f32)) -> Vec3 {
         Vec3{x: coords.0, y: coords.1, z: coords.2}
     }
 }
 
-struct Point {
-    coords: Vec3,
+impl Clone for Vec3 {
+    fn clone(&self) -> Vec3 {
+        Vec3::new((self.x, self.y, self.z))
+    }
+}
+
+#[derive(Debug)]
+pub struct Point {
+    pub coords: Vec3,
 }
 
 impl Point {
@@ -34,11 +45,22 @@ impl Point {
     }
 }
 
-struct Face {
-    verts: Vec<Vec3>
+impl Clone for Point {
+    fn clone(&self) -> Point {
+        Point::new((self.coords.x, self.coords.y, self.coords.z))
+    }
+}
+
+#[derive(Debug)]
+pub struct Face {
+    pub verts: Vec<Point>
 }
 
 impl Face {
+    fn new(first: Point, second: Point, third: Point) -> Face{
+        Face{verts: vec![first, second, third]}
+    }
+
     fn compute_normal(&self) -> Vec3 {
         // NOT YET IMPLEMENTED
         panic!("Kuso FUCK OFF")
@@ -50,16 +72,33 @@ impl Face {
     }
 }
 
-fn generate_mesh () {
-
+pub fn generate_mesh (img: &DynamicImage, settings: &GenSettings) -> Vec<Face>{
+    let bounds = img.dimensions();
+    let bounds = (bounds.0 -1, bounds.1 -1);
+    // generate main part
+    let mut faces : Vec<Face> = Vec::new();
+    for y in 0..(bounds.1 - 1) {
+        for x in 0..(bounds.0 - 1) {
+            let point0 = Point::new(((x as f32) + 0.5, (y as f32) + 0.5, compute_height(img.get_pixel(x, 0).channels4().0, &settings)));
+            let point1 = Point::new(((x as f32) + 0.5, (y as f32) + 1.5, compute_height(img.get_pixel(x, y + 1).channels4().0, &settings)));
+            let point2 = Point::new(((x as f32) + 1.5, (y as f32) + 0.5, compute_height(img.get_pixel(x + 1, y).channels4().0, &settings)));
+            let point3 = Point::new(((x as f32) + 1.5, (y as f32) + 1.5, compute_height(img.get_pixel(x + 1, y + 1).channels4().0, &settings)));
+            let face0 = Face::new(point0, point1.clone(), point2.clone());
+            let face1 = Face::new(point2, point1, point3);
+            faces.push(face0);
+            faces.push(face1);
+        }
+    }
+    faces
 }
 
-struct Extrema {
+#[derive(Debug)]
+pub struct Extrema {
     min: u8,
     max: u8
 }
 
-fn get_image_extrema(img: &DynamicImage) -> Extrema {
+pub fn get_image_extrema(img: &DynamicImage) -> Extrema {
     let mut e = Extrema{min: 255, max: 0};
     for pixel in img.pixels() {
         // only get the red channel, since all images should be monochrome
@@ -72,4 +111,15 @@ fn get_image_extrema(img: &DynamicImage) -> Extrema {
         }
     }
     e
+}
+
+pub fn get_capture_height(ext: &Extrema) -> u8 {
+    if 255 > ext.max {
+        return ext.max
+    }
+    255
+}
+
+pub fn compute_height(img_value: u8, settings: &settings::GenSettings) -> f32 {
+    ((img_value / 255) as f32) * (settings.radius as f32) * (settings.img_height_mult)
 }
---
+++ src/settings.rs
@@ -0,0 +1,122 @@
+use std::io;
+
+const DEF_RADIUS: u32 = 64;
+
+#[derive(Debug)]
+pub struct GenSettings {
+    pub radius: u32,
+    pub repeat: ImgRepeat,
+    pub height_setting: CaptureHeight,
+    pub img_height_mult: f32
+}
+
+impl GenSettings {
+    pub fn new_from_input() -> GenSettings{
+        let radius = GenSettings::get_radius_input();
+        let repeat = GenSettings::get_repeat_input();
+        let height_setting = GenSettings::get_height_input();
+        let height_mult = GenSettings::get_height_mult();
+        GenSettings::new_from_values((radius, repeat, height_setting, height_mult))
+    }
+
+    pub fn new_from_values(values: (u32, ImgRepeat, CaptureHeight, f32)) -> GenSettings{
+        GenSettings{
+            radius: values.0,
+            repeat: values.1,
+            height_setting: values.2,
+            img_height_mult: values.3
+        }
+    }
+
+    fn get_radius_input() -> u32 {
+        let mut radius = String::new();
+        println!("Please input search radius (preferably power of two), default is {}.", DEF_RADIUS);
+        io::stdin().read_line(&mut radius).expect("Failed to read input");
+        if radius.trim().len() == 0 {
+            println!("Setting {} as a radius.", DEF_RADIUS);
+            return DEF_RADIUS
+        }
+        match radius.trim().parse::<u32>() {
+            Ok(value) => value,
+            Err(_) => {
+                eprintln!("Invalid number, setting {} as a radius", DEF_RADIUS);
+                DEF_RADIUS
+            }
+        }
+    }
+
+    fn get_repeat_input() -> ImgRepeat {
+        println!("Please input image repeat option, default is Repeat.");
+        println!("1 - Repeat, 2 - Clamp, 3 - Mirror");
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Failed to read input");
+        if input.trim().len() == 0 {
+            println!("Setting Repeat.");
+            return ImgRepeat::Repeat
+        }
+        match input.trim().parse::<u8>() {
+            Ok(value) => {
+                match value {
+                    1 => ImgRepeat::Repeat,
+                    2 => ImgRepeat::Clamp,
+                    3 => ImgRepeat::Mirror,
+                    _ => {
+                        eprintln!("Invalid option, setting Repeat");
+                        ImgRepeat::Repeat
+                    }
+                }
+            },
+            Err(_) => {
+                eprintln!("Invalid input, setting Repeat");
+                ImgRepeat::Repeat
+            }
+        }
+    }
+
+    fn get_height_input() -> CaptureHeight {
+        println!("Please input capture height, or press enter for automatic computation.");
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Failed to read input");
+        if input.trim().len() == 0 {
+            println!("Setting automatic.");
+            return CaptureHeight::Generated
+        }
+        match input.trim().parse::<u8>() {
+            Ok(value) => CaptureHeight::UserDefined(value),
+            Err(_) => {
+                eprintln!("Invalid input, setting automatic");
+                CaptureHeight::Generated
+            }
+        }
+    }
+
+    fn get_height_mult() -> f32 {
+        println!("Please input height multipiler, 1.0 is radius height and default.");
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Failed to read input");
+        if input.trim().len() == 0 {
+            println!("Setting 1.0x.");
+            return 1.0
+        }
+        match input.trim().parse::<f32>() {
+            Ok(value) => value,
+            Err(_) => {
+                eprintln!("Invalid value, setting 1.0x.");
+                1.0
+            }
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum ImgRepeat {
+    Repeat,
+    Clamp,
+    Mirror
+}
+
+#[derive(Debug)]
+pub enum CaptureHeight {
+    UserDefined(u8),
+    Generated
+}
